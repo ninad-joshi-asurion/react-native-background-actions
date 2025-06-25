f6779339037d083def14e35bc54059c5de6d3b2c
ninad-joshi-asurion <149790223+ninad-joshi-asurion@users.noreply.github.com>
2025-06-25 20:54:14 +0530

    setting notification priority as min

--- android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
+++ android/src/main/java/com/asterinet/react/bgactions/RNBackgroundActionsTask.java
@@ -54,7 +54,7 @@ final public class RNBackgroundActionsTask extends HeadlessJsTaskService {
                 .setSmallIcon(iconInt)
                 .setContentIntent(contentIntent)
                 .setOngoing(true)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setPriority(NotificationCompat.PRIORITY_MIN)
                 .setForegroundServiceBehavior(NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE)
                 .setColor(color);
 
@@ -95,7 +95,7 @@ final public class RNBackgroundActionsTask extends HeadlessJsTaskService {
 
     private void createNotificationChannel(@NonNull final String taskTitle, @NonNull final String taskDesc) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final int importance = NotificationManager.IMPORTANCE_HIGH;
+            final int importance = NotificationManager.IMPORTANCE_MIN;
             final NotificationChannel channel = new NotificationChannel(CHANNEL_ID, taskTitle, importance);
             channel.setDescription(taskDesc);
             final NotificationManager notificationManager = getSystemService(NotificationManager.class);
